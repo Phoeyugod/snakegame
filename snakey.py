3f2601e56868ddf457e710650ccf0b29891a6de0
phoeyugod <hujberenzomeliton@turr.hu>
2023-12-12 08:33:08 +0100

    secondstep

--- snakey.py
+++ snakey.py
@@ -9,4 +9,18 @@ snake = [(0, 0)]
 
 food = (random.randint(0, width - 1), random.randint(0, height - 1))
  
+direction = (1, 0)
+ 
+while True:
+    for i in range(height):
+        for j in range(width):
+            if (i, j) in snake:
+                print("X", end=" ")
+            elif (i, j) == food:
+                print("F", end=" ")
+            else:
+                print(".", end=" ")
+        print()
+ 
 
+    
